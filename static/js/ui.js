40156996159b5fc94b77ed9b977a6b0550cff2b9
Andy Lin <andy010629@gmail.com>
2021-03-31 09:04:05 +0800

    css chg

--- static/js/ui.js
+++ static/js/ui.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // nav menu
-  const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'right'});
-  // add recipe form
-  const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+    // nav menu
+    const menus = document.querySelectorAll('.side-menu');
+    var sidemenu = M.Sidenav.init(menus, { edge: 'right' });
+    var elems = document.querySelectorAll('.login-page');
+    var instances = M.Modal.init(elems);
+
 });
